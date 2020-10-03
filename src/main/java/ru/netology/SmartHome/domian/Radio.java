9937dbb0da28ca188fb5244287cb087c0cf88bd2
Ekaterina Popova <katerina.shavonsky@yandex.ru>
2020-10-03 18:34:34 +0300

    Radio

---
+++ src/main/java/ru/netology/SmartHome/domian/Radio.java
@@ -0,0 +1,82 @@
+package ru.netology.SmartHome.domian;
+
+public class Radio {
+    private int radioStation;
+    private int soundVolume;
+    int[] numberStation = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int[] volume = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    public int getRadioStation() {
+        return radioStation;
+    }
+
+    public void setRadioStation(int radioStation) {
+        if (radioStation > numberStation[9]) {
+            this.radioStation = numberStation[9];
+            return;
+        }
+        if (radioStation < numberStation[0]) {
+            this.radioStation = numberStation[0];
+            return;
+        }
+        this.radioStation = radioStation;
+    }
+
+    public int getSoundVolume() {
+        return soundVolume;
+    }
+
+    public void setSoundVolume(int soundVolume) {
+        if (soundVolume > volume[10]) {
+            this.soundVolume = volume[10];
+            return;
+        }
+        if (soundVolume < volume[0]) {
+            this.soundVolume = volume[0];
+            return;
+        }
+        this.soundVolume = soundVolume;
+    }
+
+    public int[] getNumberStation() {
+        return numberStation;
+    }
+
+    public void setNumberStation(int[] numberStation) {
+        this.numberStation = numberStation;
+    }
+
+    public int[] getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int[] volume) {
+        this.volume = volume;
+        {
+            return;
+        }
+
+    }
+
+    public void numberRadioStation() {
+        if (numberStation[9] <= radioStation) {
+            radioStation = numberStation[0];
+            return;
+        }
+        if (numberStation[0] >= radioStation) {
+            radioStation = numberStation[9];
+            return;
+        }
+    }
+
+    public void shouldSoundVolume() {
+        if (soundVolume == volume[10]) {
+            soundVolume = volume[10];
+            return;
+        }
+        if (soundVolume == volume[0]) {
+            soundVolume = volume[0];
+            return;
+        }
+    }
+}
---
+++ src/test/java/ru/netology/SmartHome/domian/RadioTest.java
@@ -0,0 +1,98 @@
+package ru.netology.SmartHome.domian;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    Radio radio = new Radio();
+
+    @Test
+    void numberRadioStation() {
+        radio.setRadioStation(9);
+        radio.numberRadioStation();
+        int expected = 0;
+        assertEquals(expected, radio.getRadioStation());
+    }
+
+    @Test
+    void shouldSoundVolume() {
+        radio.setSoundVolume(10);
+        radio.shouldSoundVolume();
+        int expected = 10;
+        assertEquals(expected, radio.getSoundVolume());
+    }
+
+    @Test
+    void numberRadioStation2() {
+        radio.setRadioStation(-7);
+        radio.numberRadioStation();
+        int expected = 9;
+        assertEquals(expected, radio.getRadioStation());
+    }
+
+    @Test
+    void shouldSoundVolume2() {
+        radio.setSoundVolume(3);
+        radio.shouldSoundVolume();
+        int expected = 3;
+        assertEquals(expected, radio.getSoundVolume());
+    }
+
+    @Test
+    void numberRadioStation3() {
+        radio.setRadioStation(0);
+        radio.numberRadioStation();
+        int expected = 9;
+        assertEquals(expected, radio.getRadioStation());
+    }
+
+    @Test
+    void shouldSoundVolume3() {
+        radio.setSoundVolume(0);
+        radio.shouldSoundVolume();
+        int expected = 0;
+        assertEquals(expected, radio.getSoundVolume());
+    }
+
+    @Test
+    void numberRadioStation4() {
+        radio.setRadioStation(11);
+        radio.numberRadioStation();
+        int expected = 0;
+        assertEquals(expected, radio.getRadioStation());
+    }
+
+    @Test
+    void shouldSoundVolume4() {
+        radio.setSoundVolume(15);
+        radio.shouldSoundVolume();
+        int expected = 10;
+        assertEquals(expected, radio.getSoundVolume());
+    }
+
+    @Test
+    void numberRadioStation5() {
+        radio.setRadioStation(1);
+        radio.numberRadioStation();
+        int expected = 1;
+        assertEquals(expected, radio.getRadioStation());
+    }
+
+    @Test
+    void shouldSoundVolume5() {
+        radio.setSoundVolume(0);
+        radio.shouldSoundVolume();
+        int expected = 0;
+        assertEquals(expected, radio.getSoundVolume());
+    }
+
+    @Test
+    void shouldSoundVolume6() {
+        radio.setSoundVolume(-1);
+        radio.shouldSoundVolume();
+        int expected = 0;
+        assertEquals(expected, radio.getSoundVolume());
+    }
+}
